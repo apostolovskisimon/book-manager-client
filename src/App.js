2db2a35674e4e39ca8dfdbe80aadf182ae5ae3e4
simonapostolovski <simon.apostolovski@atis.al>
2021-03-25 14:19:18 +0100

    startup book app master

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  BrowserRouter as Router,
+  Route,
+  // Link,
+  // useHistory,
+  Switch,
+} from "react-router-dom";
+import { Login } from "./Components/Login/Login";
+import { Nav } from "./Components/Nav/Nav";
+import { Signup } from "./Components/Signup/Signup";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Nav />
+      <Switch>
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/signup" component={Signup} />
+      </Switch>
+    </Router>
   );
 }
 
